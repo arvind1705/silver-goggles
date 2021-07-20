5d566591bccb23ba069695d4f9671dd04856647c
Aravind G <argunasekaran@deloitte.com>
2021-07-20 08:57:57 +0530

    Add models to store Youtube video and thumbnail information.

--- fampay/search/models.py
+++ fampay/search/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class YoutubeVideo(models.Model):
+    video_id = models.CharField(max_length=50, primary_key=True)
+    video_title = models.CharField(max_length=100, db_index=True)
+    video_description = models.TextField(db_index=True)
+    publishing_datetime = models.DateTimeField()
+    inserted_at = models.DateTimeField(auto_now_add=True)
+
+
+class ThumbnailURL(models.Model):
+    url = models.URLField()
+    resolution_type = models.CharField(max_length=10, choices=[
+        ('default', 'Default'),
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ], default='default')
+    yt_video = models.ForeignKey(YoutubeVideo, on_delete=models.CASCADE)
